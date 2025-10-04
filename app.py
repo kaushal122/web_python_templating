dfb4988c644155c6781635990cc8b82dad700f40
Kaushal Kishor <radhe@Kaushals-MacBook-Air.local>
2025-10-04 23:59:21 +0530

    Load page based on the weekend or weekday

--- app.py
+++ app.py
@@ -9,20 +9,13 @@ app = Flask(__name__)
 @app.route('/')
 def daily_message():
     now = datetime.datetime.now()
+    print(now)
     day_name = now.strftime("%A")
 
     if day_name in ['Saturday', 'Sunday']:
         message = "It's the weekend! Enjoy your day! or Side Hustle Time!"
         theme = "weekend"
-    else:
-        task = random.choice(["Stay focused!", "Keep pushing!", "You got this!", "Mail Check", "Walking Time or Stretch Time or Hydration Time"])
-        message = f"Happy {day_name}! {task}  Stay productive!"
-        theme = "weekday"
-    return render_template("day.html", day=day_name, message=message, theme=theme)
-
-@app.route('/sidehustle')
-def side_hustle():
-    Side_hustle_ideas=["Free lancing",
+        Side_hustle_ideas=["Free lancing",
                        "Blogging",
                        "Tutoring",
                        "Reading Novels",
@@ -30,8 +23,15 @@ def side_hustle():
                        "Learning trading",
                        "Selling handmade crafts",
                        "Dropshipping",]
-    idea= random.choice(Side_hustle_ideas)
-    return render_template("sidehustle.html",idea=idea)
+        idea= random.choice(Side_hustle_ideas)
+        return render_template("sidehustle.html",idea=idea, day=day_name, message=message, theme=theme)
+    else:
+        task = random.choice(["Stay focused!", "Keep pushing!", "You got this!", "Mail Check", "Walking Time or Stretch Time or Hydration Time"])
+        message = f"Happy {day_name}! {task}  Stay productive!"
+        theme = "weekday"
+        return render_template("day.html", day=day_name, message=message, theme=theme)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
